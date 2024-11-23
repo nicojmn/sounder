dbd408e1ba3f3200ee3c5c6e47a529467d038989
nicojmn <nicolasjeanmenne@gmail.com>
2024-11-23 10:41:03 +0100

    main page

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,24 @@
 # Packages and built-in modules
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
 
 # Sounder modules
 from routers import auth
 
 
-
-app = FastAPI()
+app = FastAPI(
+    debug=True,
+    title="Sounder",
+    license_info={
+        "name": "AGPLv3",
+        "url": "https://www.gnu.org/licenses/agpl-3.0.html"
+    }
+)
 
 # import routers here
 app.include_router(auth.router)
 
 # root route
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=FileResponse)
 async def read_root(request: Request):
-    return HTMLResponse(content="Waiting for front team", status_code=200) # TODO : create file in frontend
+    return FileResponse("../frontend/build/index.html")
