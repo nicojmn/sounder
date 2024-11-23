945cfdba82ade1598544284803f9272fc0b969f6
nicojmn <nicolasjeanmenne@gmail.com>
2024-11-23 04:53:25 +0100

    Frontend skeleton

---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "$lib/app.css";
+</script>
+
+<div role="alert" class="alert alert-warning">
+    <span>This webapp is under construction</span>
+</div>
+  
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to Sounder</h1>
+<p>Sounder is a music webapp that let you discover new music</p>
