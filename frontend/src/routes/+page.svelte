dc757b42a57809d8754f2b084010455117acc13a
nicojmn <nicolasjeanmenne@gmail.com>
2024-11-23 15:56:28 +0100

    Yes

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to Sounder</h1>
-<p>Sounder is a music webapp that let you discover new music</p>
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import deezer_logo from "$lib/assets/images/deezer.svg";
+    import "$lib/components/CoverStack.svelte";
+	import CoverStack from "$lib/components/CoverStack.svelte";
+    
+    let demo: boolean = true;
+</script>
+
+
+<div class="flex items-center justify-center">
+    <CoverStack {demo}/>
+    <button class="btn-circle btn-neutral  w-1/5" type="button" on:click={() => {
+        goto("/deezer");
+    }}>
+        <img class="w-full h-full" src={deezer_logo} alt="Deezer logo" />
+    </button>
+</div>
