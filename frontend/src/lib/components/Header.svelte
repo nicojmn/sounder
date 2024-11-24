22542ff93fd34d2b9a2546b0d8959ae0f31fcd17
nicojmn <nicolasjeanmenne@gmail.com>
2024-11-24 14:56:14 +0100

    Almost almost almost demo ready

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import '$lib/app.css';
+
+	let demo: boolean = true;
 </script>
 
 
-<div role="alert" class="alert alert-warning">
-	<span>🚧 This webapp is under construction</span>
-</div>
+{#if !demo}
+	<div role="alert" class="alert alert-warning">
+		<span>🚧 This webapp is under construction</span>
+	</div>
+{/if}
+
 <div class="navbar bg-base-100">
 	<div class="flex-1 justify-center">
 		<a class="btn btn-ghost text-xl">Sounder</a>
